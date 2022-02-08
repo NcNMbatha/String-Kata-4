b49dc1a46b2cfb7b2c5970ff9ac5f9e86b4fed67
Njabulo Mbatha <njabulo.mbatha@kcpivot.co.za>
2022-02-08 16:00:00 +0200

    First Commit after Coach review

--- KataAttemptFour/AttemptFour.cs
+++ KataAttemptFour/AttemptFour.cs
@@ -6,72 +6,64 @@
         {
             if (string.IsNullOrEmpty(numbers))
                 return 0;
-            if (CountNegetiveNumbers(numbers) > 0)
-            {
-                NegetiveNumbersException(ExtractNumbersFromTextInput(numbers));
-            }
+
+            NegetiveNumbersException(ExtractNumbersFromTextInput(numbers));
             return SumOfAllNumbers(numbers);
         }
-        public int CountNegetiveNumbers(string numbersToCount)
-        {
-            return (from number in ExtractNumbersFromTextInput(numbersToCount)
-                    where number < 0
-                    select number).Count();
-        }
+
         public int SumOfAllNumbers(string numbersToAdd)
         {
-            return FilteredArrayOfNumbers(numbersToAdd).Sum();
+            int biggestAllowedNumber = 1000;
+            return (from number in ExtractNumbersFromTextInput(numbersToAdd)
+                    where number <= biggestAllowedNumber
+                    select number).Sum();
         }
-        public List<int> FilteredArrayOfNumbers(string numbersToFilter)
-        {
-            return (from number in ExtractNumbersFromTextInput(numbersToFilter)
-                    where number <= 1000
-                    select number).ToList();
-        }
+
         public List<int> ExtractNumbersFromTextInput(string NumbersToExtract)
         {
-            List<int> arrayOfNumbers = new List<int>();
+            int isInteger = 0;
             string[] numbersArray = NumbersToExtract.Split(DelimeterList(NumbersToExtract).ToArray(), StringSplitOptions.None);
-            foreach (var number in numbersArray)
-            {
-                if (int.TryParse(number, out int num))
-                {
-                    arrayOfNumbers.Add(num);
-                }
-            }
-            return arrayOfNumbers;
+            return numbersArray.Where(x => int.TryParse(x, out isInteger)).Select(x => isInteger).ToList();
         }
+
         public List<string> DelimeterList(string numbersWithDelimeters)
         {
-            List<string> list = new List<string>() { ",", "\n" };
+            List<string> delimeterList = new List<string>() { ",", "\n" };
             if (numbersWithDelimeters.StartsWith("//"))
             {
                 if (numbersWithDelimeters.Substring(2, 1) == "[")
-                    return list.Union(DelimetersInBrackets(numbersWithDelimeters)).ToList();
+                    return delimeterList.Union(DelimetersInBrackets(numbersWithDelimeters)).ToList();
 
-                var splitOutput = numbersWithDelimeters.Split("\n");
-                int index = splitOutput[0].Length;
-                list.Add(splitOutput[0].Substring(2, index - 2));
+                var numbersWithDelimetersSplit = numbersWithDelimeters.Split("\n");
+                int index = numbersWithDelimetersSplit[0].Length;
+                delimeterList.Add(numbersWithDelimetersSplit[0].Substring(2, index - 2));
             }
-            return list;
+            return delimeterList;
         }
+
         public List<string> DelimetersInBrackets(string numbersWithDelimeters)
         {
-            int index = numbersWithDelimeters.LastIndexOf("]");
-            string delimeter = numbersWithDelimeters.Substring(3, index - 3).Trim(']');
+            int indexOfClosingSqureBracket = numbersWithDelimeters.LastIndexOf("]");
+            string delimeter = numbersWithDelimeters.Substring(3, indexOfClosingSqureBracket - 3).Trim(']');
             return delimeter.Split("][").ToList();
         }
-        public Exception NegetiveNumbersException(List<int> arrayOfNumbers)
+
+        public void NegetiveNumbersException(List<int> arrayOfNumbers)
         {
-            string negetives = "Negatives not allowed.";
+            int count = 0;
+            string negetives = "Negetives not allowed.";
             foreach (var item in arrayOfNumbers)
             {
                 if (item < 0)
                 {
                     negetives += "\n" + item;
+                    count++;
                 }
             }
-            throw new Exception(negetives);
+            if (count > 0)
+            {
+                throw new Exception(negetives);
+            }
         }
     }
 }
--- KataAttemptFourTest/AttemptFourTest.cs
+++ KataAttemptFourTest/AttemptFourTest.cs
@@ -6,78 +6,78 @@ namespace KataAttemptFourTest
 {
     public class AttemptFourTest
     {
-        AttemptFour kata;
+        AttemptFour attemptFour;
         [SetUp]
         public void Setup()
         {
-            kata = new AttemptFour();
+            attemptFour = new AttemptFour();
         }
 
         [Test]
-        public void Given_EmptyString_When_Adding_Should_Return_0()
+        public void Given_EmptyString_When_Adding_Should_Return_Zero()
         {
-            int results = kata.Add("");
+            int results = attemptFour.Add("");
             int expected = 0;
             Assert.AreEqual(expected, results);
         }
         [Test]
-        public void Given_InputString_1_When_Adding_Should_Return_1()
+        public void Given_OneNumberWhen_Adding_Should_Return_One()
         {
-            int results = kata.Add("1");
+            int results = attemptFour.Add("1");
             int expected = 1;
             Assert.AreEqual(expected, results);
         }
         [Test]
-        public void Given_InputString_1_And_2_SeperatedByDelimeter_When_Adding_Should_Return_3()
+        public void Given_TwoNumbers_SeperatedByDelimeter_When_Adding_Should_Return_SumOfTwoNumbers()
         {
-            int results = kata.Add("1,2");
+            int results = attemptFour.Add("1,2");
             int expected = 3;
             Assert.AreEqual(expected, results);
         }
         [Test]
-        public void Given_InputString_1_2_And_3_SeperatedByDelimeters_When_Adding_Should_Return_6()
+        public void Given_ThreeNumbers_SeperatedByDelimeters_When_Adding_Should_SumOfThreeNumbers()
         {
-            int results = kata.Add("1\n2,3");
+            int results = attemptFour.Add("1\n2,3");
             int expected = 6;
             Assert.AreEqual(expected, results);
         }
         [Test]
-        public void Given_InputStringStartsWithDoubleForwardSlashes_When_Adding_Should_Return_3()
+        public void Given_TwoNumbersStartingWithForwardSlashes_When_Adding_Should_Return_SumOfTwoNumbers()
         {
-            int results = kata.Add("//;\n1;2");
+            int results = attemptFour.Add("//;\n1;2");
             int expected = 3;
             Assert.AreEqual(expected, results);
         }
         [Test]
-        public void Given_InputStringWithNegetiveNumbers_When_Adding_Should_ThrowException()
+        public void Given_NumbersWithNegetives_When_Adding_Should_ThrowException()
         {
-            Assert.Throws<Exception>(() => kata.Add("1\n2,-3"));
+            Assert.Throws<Exception>(() => attemptFour.Add("1\n2,-3"));
         }
         [Test]
-        public void Given_InputStringWithNumbers_GreaterThan_1000_When_Adding_Should_Return_3()
+        public void Given_NumbersGreaterThan1000_When_Adding_Should_Return_SumOfAllNumbersExcludingNumbersGreaterThan1000()
         {
-            int results = kata.Add("//;\n1;2,2000");
+            int results = attemptFour.Add("//;\n1;2,2000");
             int expected = 3;
             Assert.AreEqual(expected, results);
         }
         [Test]
-        public void Given_InputStringWithDelimiters_OfDifferentLength_When_Adding_Should_Return_6()
+        public void Given_NumbersSeperatedByDelimiters_OfDifferentLength_When_Adding_Should_Return_SumOfAllNumbers()
         {
-            int results = kata.Add("//%%%\n1%%%2%%%3");
+            int results = attemptFour.Add("//%%%\n1%%%2%%%3");
             int expected = 6;
             Assert.AreEqual(expected, results);
         }
         [Test]
-        public void Given_InputStringWithMultiplyDelimiters_InSquareBrackets_When_Adding_Should_Return_6()
+        public void Given_NumbersSeperatedByMultiplyDelimiters_InSquareBrackets_When_Adding_Should_Return_SumOfAllNumbers()
         {
-            int results = kata.Add("//[*][%]\n1*2%3");
+            int results = attemptFour.Add("//[*][%]\n1*2%3");
             int expected = 6;
             Assert.AreEqual(expected, results);
         }
         [Test]
-        public void Given_InputStringWithMultiplyDelimitersOfDifferentLength_InSquareBrackets_When_Adding_Should_Return_6()
+        public void Given_NumbersSeperatedByMultiplyDelimitersOfDifferentLength_InSquareBrackets_When_Adding_Should_Return_SumOfAllNumbers()
         {
-            int results = kata.Add("//[***][%%%%]\n1***2%%%%3");
+            int results = attemptFour.Add("//[***][%%%%]\n1***2%%%%3");
             int expected = 6;
             Assert.AreEqual(expected, results);
         }
